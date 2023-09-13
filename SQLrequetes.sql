82e41ae402a879a7d03a68ff9102e00d34cc984b
Saveliy <savelii9000@gmail.com>
2023-09-13 11:37:20 +0200

    alimentation queries added

--- SQLrequetes.sql
+++ SQLrequetes.sql
@@ -1,4 +1,8 @@
+//made for mariaDB on mysql.
+
+
 CREATE DATABASE cinema_database;
+
 
 CREATE TABLE clients(
   id INT(50) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -27,9 +31,9 @@ CREATE TABLE sessions(
   id INT(50) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   movieId INT(50) NOT NULL,
   cinemaId INT(50) NOT NULL,
-  date DATE NOT NULL,
+  date DATETIME NOT NULL,
   places INT(10) NOT NULL,
-  placesLeft INT(10) NOT NULL,
+  placesLeft INT(10) NOT NULL DEFAULT places,
   room VARCHAR(50) NOT NULL,
   FOREIGN KEY (movieId) REFERENCES movies(id),
   FOREIGN KEY (cinemaId) REFERENCES cinemas(id)
@@ -44,3 +48,53 @@ CREATE TABLE reservations(
   FOREIGN KEY (sessionId) REFERENCES sessions(id),
   FOREIGN KEY (priceId) REFERENCES prices(id)
 );
+
+
+
+
+
+INSERT INTO 
+	movies(name)
+VALUES 
+	('Fast and curious'),
+    ('Prider man'),
+    ('Bad man'),
+    ('Man man');
+
+INSERT INTO 
+    cinemas(name)
+VALUES 
+    ('Ocean view'),
+    ('fun films'),
+    ('not so fun films');
+
+INSERT INTO 
+    clients(firstName,lastName,mail)
+VALUES 
+    ('Jon','Doe','john@gmail.com'),
+    ('Damien','Lebrun','redred@gmail.com'),
+    ('Chingin','Guanko','chinginredgmail.com'),
+    ('Dmitry','Potapov','yellowblueorange@gmail.com');
+
+INSERT INTO 
+    prices(ticketType,price)
+VALUES 
+    ('Plein tarif','9.20'),
+    ('Etudiant','7.60'),
+    ('Moins de 14 ans','5.90');
+
+INSERT INTO 
+    sessions(movieId,cinemaId,date,places,room)
+VALUES 
+    ('1','2','2023-09-14 12:15:00','250','206 A'),
+    ('2','2','2023-09-14 12:15:00','250','106 B'),
+    ('1','3','2023-09-24 14:30:00','250','206 A'),
+    ('2','3','2023-09-24 14:30:00','250','106 B');
+
+
+INSERT INTO 
+    reservations(clientId,sessionId,priceId)
+VALUES 
+    ('1','1','1'),
+    ('2','1','2'),
+    ('4','4','1');
